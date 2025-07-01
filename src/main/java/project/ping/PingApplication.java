b286073e9f02ca978e40fd89190b2c00841796f0
ssongmina <thdalsdk5498@naver.com>
2025-07-01 15:54:19 +0900

    [FEAT] EntityListeners 어노테이션 추가

--- src/main/java/project/ping/PingApplication.java
+++ src/main/java/project/ping/PingApplication.java
@@ -1,9 +1,12 @@
 package project.ping;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @SpringBootApplication
+@EntityListeners(AuditingEntityListener.class)
 public class PingApplication {
 
 	public static void main(String[] args) {
