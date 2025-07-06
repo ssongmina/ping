178fe9441ef52cec71c25aa4dc8c0e0726fe2024
ssongmina <thdalsdk5498@naver.com>
2025-07-06 14:54:47 +0900

    [FEAT] JWT 토큰 유효성 검증, 인증 객체 생성 등 메서드 작성

--- src/main/java/project/ping/security/jwt/JwtTokenProvider.java
+++ src/main/java/project/ping/security/jwt/JwtTokenProvider.java
@@ -1,12 +1,20 @@
 package project.ping.security.jwt;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import project.ping.apiPayload.exception.GeneralException;
+import project.ping.apiPayload.status.ErrorStatus;
 import project.ping.dto.JwtDTO;
+import project.ping.security.auth.MemberDetail;
+import project.ping.security.auth.MemberDetailService;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
@@ -14,7 +22,10 @@ import java.util.Date;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class JwtTokenProvider implements InitializingBean {
+
+    private final MemberDetailService memberDetailService;
 
     @Value("${spring.jwt.secret}")
     private String secret;
@@ -60,6 +71,54 @@ public class JwtTokenProvider implements InitializingBean {
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .build();
+    }
+
+    // 요청 헤더에서 토큰 뽑아내기
+    public String resolveToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if(header == null || !header.startsWith("Bearer ")){
+            return null;
+        }
+        return header.substring(7).trim();
+    }
+
+    // 토큰 유효성 검증
+    public boolean validateToken(String token) {
+        try{
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        }catch (SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+            throw new GeneralException(ErrorStatus.WRONG_TYPE_SIGNATURE);
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+            throw new GeneralException(ErrorStatus.TOKEN_EXPIRED);
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+            throw new GeneralException(ErrorStatus.WRONG_TYPE_TOKEN);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+            throw new GeneralException(ErrorStatus.NOT_VALID_TOKEN);
+        }
+    }
+
+    // 토큰을 복호화하여 claim 정보를 통해 인증 객체(Authentication) 생성
+    public Authentication getAuthentication(String token) {
+        MemberDetail memberDetail = memberDetailService.loadUserByUsername(this.getEmail(token));
+        return new UsernamePasswordAuthenticationToken(memberDetail, null, memberDetail.getAuthorities());
+    }
+
+    // 토큰에서 이메일 추출
+    public String getEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("email", String.class);
     }
 
 }
