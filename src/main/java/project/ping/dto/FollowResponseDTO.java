1e072978b0f5df1c85136917e9dd8c86cece78bd
ssongmina <thdalsdk5498@naver.com>
2025-07-10 10:53:01 +0900

    [FEAT] 팔로잉 리스트를 뽑는 DTO 생성

---
+++ src/main/java/project/ping/dto/FollowResponseDTO.java
@@ -0,0 +1,29 @@
+package project.ping.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class FollowResponseDTO {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FollowingListDTO{
+        private Long count;
+        private List<FollowingDTO> followingDTOList;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FollowingDTO{
+        private Long followingId;
+        private String followingName;
+    }
+}
