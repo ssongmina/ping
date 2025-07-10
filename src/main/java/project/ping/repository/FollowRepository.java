7e928d89463f110a475c88446acbd34f389fe475
ssongmina <thdalsdk5498@naver.com>
2025-07-10 10:53:53 +0900

    [FEAT] 팔로워로 팔로우 엔티티를 찾는 메서드 작성

--- src/main/java/project/ping/repository/FollowRepository.java
+++ src/main/java/project/ping/repository/FollowRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.ping.domain.Follow;
 import project.ping.domain.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     boolean existsByFollowerAndFollowing(Member follower, Member following);
 
     Optional<Follow> findByFollowerAndFollowing(Member follower, Member following);
+
+    List<Follow> findByFollower(Member follower);
 }
