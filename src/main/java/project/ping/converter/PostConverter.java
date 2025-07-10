ab819436a11df6958999c55ffda5a2ce0b746c54
ssongmina <thdalsdk5498@naver.com>
2025-07-10 16:36:12 +0900

    [FEAT] 게시글 정보를 담는 DTO로 변환하는 메서드 작성

--- src/main/java/project/ping/converter/PostConverter.java
+++ src/main/java/project/ping/converter/PostConverter.java
@@ -3,6 +3,10 @@ package project.ping.converter;
 import project.ping.domain.Member;
 import project.ping.domain.Post;
 import project.ping.dto.PostRequestDTO;
+import project.ping.dto.PostResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PostConverter {
 
@@ -12,5 +16,21 @@ public class PostConverter {
                 .content(request.getContent())
                 .build();
     }
+
+    public static PostResponseDTO.getPostListDTO toPostList(List<Post> posts) {
+        List<PostResponseDTO.getPostDTO> lists = posts.stream().
+                map(PostConverter::toPostDTO).collect(Collectors.toList());
+        return PostResponseDTO.getPostListDTO.builder()
+                .lists(lists)
+                .build();
+    }
+
+    public static PostResponseDTO.getPostDTO toPostDTO(Post post){
+        return PostResponseDTO.getPostDTO.builder()
+                .id(post.getId())
+                .content(post.getContent())
+                .build();
+    }
+
 
 }
