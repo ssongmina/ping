76a0f51d210ab2f621962d373f92e669a234d302
ssongmina <thdalsdk5498@naver.com>
2025-07-10 10:53:46 +0900

    [FEAT] 팔로워로 팔로우 엔티티를 찾는 메서드 작성

--- src/main/java/project/ping/converter/FollowConverter.java
+++ src/main/java/project/ping/converter/FollowConverter.java
@@ -2,6 +2,10 @@ package project.ping.converter;
 
 import project.ping.domain.Follow;
 import project.ping.domain.Member;
+import project.ping.dto.FollowResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FollowConverter {
 
@@ -9,6 +13,23 @@ public class FollowConverter {
         return Follow.builder()
                 .follower(follower)
                 .following(following)
+                .build();
+    }
+
+    public static FollowResponseDTO.FollowingListDTO toFollowList(List<Follow> follows) {
+        List<FollowResponseDTO.FollowingDTO> lists = follows.stream()
+                .map(FollowConverter::toFollowingDTO).collect(Collectors.toList());
+        return FollowResponseDTO.FollowingListDTO.builder()
+                .count(lists.stream().count())
+                .followingDTOList(lists)
+                .build();
+    }
+
+    public static FollowResponseDTO.FollowingDTO toFollowingDTO(Follow follow){
+        Member member = follow.getFollowing();
+        return FollowResponseDTO.FollowingDTO.builder()
+                .followingId(member.getId())
+                .followingName(member.getNickname())
                 .build();
     }
 }
