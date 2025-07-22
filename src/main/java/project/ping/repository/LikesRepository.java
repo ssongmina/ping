6bedf41ca0201818c612e8dfcceb78b2271cdd5c
ssongmina <thdalsdk5498@naver.com>
2025-07-22 09:55:30 +0900

    [FEAT] 해당 게시글에 눌러진 좋아요의 개수를 세는 메서드 작성

--- src/main/java/project/ping/repository/LikesRepository.java
+++ src/main/java/project/ping/repository/LikesRepository.java
@@ -1,6 +1,8 @@
 package project.ping.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import project.ping.domain.Likes;
 import project.ping.domain.Member;
 import project.ping.domain.Post;
@@ -8,4 +10,11 @@ import project.ping.domain.Post;
 public interface LikesRepository extends JpaRepository<Likes, Long> {
 
     Likes findByMemberAndPost(Member member, Post post);
+
+    // 해당 POST에 알맞는 LIkes 엔티티의 개수 세기
+    @Query("""
+        SELECT count(*) FROM Likes l WHERE l.post = :post
+        """
+    )
+    Long countLikes(@Param("post") Post post);
 }
