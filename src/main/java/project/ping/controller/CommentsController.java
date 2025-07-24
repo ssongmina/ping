bed51fe89e724f16f24d504bbcacb8eabf990934
ssongmina <thdalsdk5498@naver.com>
2025-07-24 15:03:28 +0900

    [FEAT] 댓글을 작성하는 컨트롤러 로직 작성

---
+++ src/main/java/project/ping/controller/CommentsController.java
@@ -0,0 +1,30 @@
+package project.ping.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.ping.apiPayload.ApiResponse;
+import project.ping.dto.CommentsRequestDTO;
+import project.ping.security.auth.MemberDetail;
+import project.ping.service.CommentsService;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
+public class CommentsController {
+
+    private final CommentsService commentsService;
+
+    @PostMapping
+    @Operation(summary = "댓글 작성하기 API")
+    public ApiResponse<?> write(@AuthenticationPrincipal MemberDetail memberDetail,
+                                @RequestBody CommentsRequestDTO.WriteCommentsDTO request){
+        commentsService.writeComments(memberDetail, request);
+        return ApiResponse.onSuccess(null);
+    }
+
+}
