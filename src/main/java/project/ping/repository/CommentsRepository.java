354c23057d0f33197ae8abdc626e981f321af548
ssongmina <thdalsdk5498@naver.com>
2025-07-25 11:01:07 +0900

    [FEAT] 게시글에 쓰여진 모든 댓글을 찾는 메서드 작성

--- src/main/java/project/ping/repository/CommentsRepository.java
+++ src/main/java/project/ping/repository/CommentsRepository.java
@@ -2,6 +2,10 @@ package project.ping.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.ping.domain.Comments;
+import project.ping.domain.Post;
+
+import java.util.List;
 
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
+    List<Comments> findByPost(Post post);
 }
